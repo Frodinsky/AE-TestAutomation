5a20c74773477259306f24ac0a37231786a55276
Frodinsky <rodolara97@gmail.com>
2025-07-27 13:04:09 -0700

    test case 1 version pro

--- tests/test_register_modular.py
+++ tests/test_register_modular.py
@@ -1,6 +1,10 @@
 from helpers.user_flows import User
+import time
+
 def test_register_new_user(driver):
 
     user = User(driver)
     user.create()
-    user.delete()
+    #user.delete()
+
+    time.sleep(5)
