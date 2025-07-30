9f981e83c8ec1b4f5bb155dcb1c70de93247cd43
Frodinsky <rodolara97@gmail.com>
2025-07-30 14:25:43 -0700

    test case 9

--- helpers/products_flows.py
+++ helpers/products_flows.py
@@ -5,8 +5,9 @@ from components.product_information_component import ProductInformation
 
 class Products:
 
-    def __init__(self, driver):
+    def __init__(self, driver, search_product="Blue Top"):
         self.driver = driver
+        self.search_product = search_product
         self.header = HeaderComponent(driver)
         self.all_products = AllProducts(driver)
 
@@ -33,5 +34,12 @@ class Products:
 
         for key, expected_value in expected.items():
             assert details[key] == expected_value, f"Expected {key} to be '{expected_value}' but got '{details[key]}'"
+
+    def search_product_bar(self):
+        assert "FEATURES ITEMS" in self.all_products.verify_feature_items_text()
+        self.header.click_products_button()
+        self.all_products.fill_serch_product_bar(self.search_product)
+        self.all_products.click_search_product_button()
+        assert "Blue Top" in self.all_products.get_product_search_text()
 
 
--- tests/test_search_product.py
+++ tests/test_search_product.py
@@ -3,4 +3,4 @@ from helpers.products_flows import Products
 def test_products(driver):
 
     products = Products(driver)
-    products.see_product()
+    products.search_product_bar()
